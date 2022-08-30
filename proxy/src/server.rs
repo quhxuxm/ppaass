fbe82058f3df1f71057e8bf782c51d0cbca7e304
Qu Hao <quhxuxm_quh@163.com>
2022-08-30 08:40:56 +0800

    init

--- proxy/src/server.rs
+++ proxy/src/server.rs
@@ -159,8 +159,8 @@ impl ProxyServer {
                             runtime.shutdown_timeout(Duration::from_secs(60));
                             return;
                         },
-                        Ok(other_sighal) => {
-                            info!("Ignore other single when proxy server is running: {:?}", other_sighal);
+                        Ok(other_signal) => {
+                            info!("Ignore other single when proxy server is running: {:?}", other_signal);
                         },
                         Err(e) => {
                             error!("Fail to receive proxy server signal because of error: {:#?}", e);
