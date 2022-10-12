cdfb7aec73a7edd116b67c9bd6e1d1cb9fbef8e4
Qu Hao <quhxuxm_quh@163.com>
2022-10-12 22:18:48 +0800

    init

--- protocol/src/message/payload.rs
+++ protocol/src/message/payload.rs
@@ -26,6 +26,44 @@ pub struct PpaassMessagePayload {
     data: Vec<u8>,
 }
 
+impl PpaassMessagePayload {
+    pub fn new(
+        source_address: Option<PpaassProtocolAddress>, target_address: Option<PpaassProtocolAddress>, payload_type: PpaassMessagePayloadType, data: Vec<u8>,
+    ) -> Self {
+        Self {
+            payload_type,
+            source_address,
+            target_address,
+            additional_info: HashMap::new(),
+            data,
+        }
+    }
+
+    pub fn get_additional_info(&self, key: &PayloadAdditionalInfoKey) -> Option<&PayloadAdditionalInfoValue> {
+        self.additional_info.get(key)
+    }
+
+    pub fn add_additional_info(&mut self, key: PayloadAdditionalInfoKey, value: PayloadAdditionalInfoValue) -> Option<PayloadAdditionalInfoValue> {
+        self.additional_info.insert(key, value)
+    }
+
+    pub fn get_source_address(&self) -> &Option<PpaassProtocolAddress> {
+        &self.source_address
+    }
+
+    pub fn get_target_address(&self) -> &Option<PpaassProtocolAddress> {
+        &self.target_address
+    }
+
+    pub fn get_payload_type(&self) -> &PpaassMessagePayloadType {
+        &self.payload_type
+    }
+
+    pub fn get_data(&self) -> &Vec<u8> {
+        &self.data
+    }
+}
+
 impl TryFrom<PpaassMessagePayload> for Vec<u8> {
     type Error = PpaassError;
 
--- protocol/tests/message.rs
+++ protocol/tests/message.rs
@@ -1,10 +1,38 @@
 use std::error::Error;
 
-use ppaass_protocol::PpaassMessagePayloadEncryption;
+use ppaass_protocol::{
+    PayloadAdditionalInfoKey, PayloadAdditionalInfoValue, PpaassMessageAgentPayloadTypeValue, PpaassMessagePayload, PpaassMessagePayloadEncryption,
+    PpaassMessagePayloadType, PpaassProtocolAddress,
+};
 
 #[test]
 fn test_serialize_message() -> Result<(), Box<dyn Error>> {
     let message = ppaass_protocol::PpaassMessage::new("user1".to_string(), PpaassMessagePayloadEncryption::Aes(vec![1, 2, 3]), vec![1, 2, 3]);
     println!("{}", serde_json::to_string(&message)?);
+    Ok(())
+}
+
+#[test]
+fn test_serialize_message_payload() -> Result<(), Box<dyn Error>> {
+    let source_address = PpaassProtocolAddress::IpV4 { ip: [1, 2, 3, 4], port: 80 };
+    let target_address = PpaassProtocolAddress::IpV4 {
+        ip: [10, 20, 30, 40],
+        port: 800,
+    };
+    let mut message_payload = PpaassMessagePayload::new(
+        Some(source_address),
+        Some(target_address),
+        PpaassMessagePayloadType::AgentPayload(PpaassMessageAgentPayloadTypeValue::TcpInitialize),
+        vec![1; 100],
+    );
+    message_payload.add_additional_info(
+        PayloadAdditionalInfoKey::ReferenceMessageId,
+        PayloadAdditionalInfoValue::ReferenceMessageIdValue("reference_message_id1".to_string()),
+    );
+    message_payload.add_additional_info(
+        PayloadAdditionalInfoKey::ReferenceMessageId,
+        PayloadAdditionalInfoValue::ReferenceMessageIdValue("reference_message_id2".to_string()),
+    );
+    println!("{}", serde_json::to_string(&message_payload)?);
     Ok(())
 }
