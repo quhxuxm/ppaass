6b431078ddc4bf8fd6e52cf87981639b1a5f3d24
Qu Hao <quhxuxm_quh@163.com>
2022-11-13 14:51:58 +0800

    refact

---
+++ protocol/src/message/payload/heartbeat.rs
@@ -0,0 +1,50 @@
+use anyhow::Context;
+use serde_derive::{Deserialize, Serialize};
+
+use crate::PpaassNetAddress;
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct HeartbeatRequestPayload {
+    pub src_address: PpaassNetAddress,
+    pub dest_address: PpaassNetAddress,
+}
+
+impl TryFrom<Vec<u8>> for HeartbeatRequestPayload {
+    type Error = anyhow::Error;
+
+    fn try_from(value: Vec<u8>) -> Result<Self, Self::Error> {
+        serde_json::from_slice(&value).context("fail generate HeartbeatRequestPayload from input bytes")
+    }
+}
+
+impl TryFrom<HeartbeatRequestPayload> for Vec<u8> {
+    type Error = anyhow::Error;
+
+    fn try_from(value: HeartbeatRequestPayload) -> Result<Self, Self::Error> {
+        serde_json::to_vec(&value).context("fail generate bytes from HeartbeatRequestPayload")
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct HeartbeatResponsePayload {
+    pub src_address: PpaassNetAddress,
+    pub dest_address: PpaassNetAddress,
+}
+
+impl TryFrom<Vec<u8>> for HeartbeatResponsePayload {
+    type Error = anyhow::Error;
+
+    fn try_from(value: Vec<u8>) -> Result<Self, Self::Error> {
+        serde_json::from_slice(&value).context("fail generate HeartbeatResponsePayload from input bytes")
+    }
+}
+
+impl TryFrom<HeartbeatResponsePayload> for Vec<u8> {
+    type Error = anyhow::Error;
+
+    fn try_from(value: HeartbeatResponsePayload) -> Result<Self, Self::Error> {
+        serde_json::to_vec(&value).context("fail generate bytes from HeartbeatResponsePayload")
+    }
+}
