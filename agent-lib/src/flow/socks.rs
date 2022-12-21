e1756a64cf831dc539bfe87a8313f373a6aeec27
Qu Hao <quhxuxm_quh@163.com>
2022-12-21 08:41:29 +0800

    refact

--- agent-lib/src/flow/socks.rs
+++ agent-lib/src/flow/socks.rs
@@ -92,6 +92,29 @@ where
             "Client tcp connection [{client_socket_address}] start socks5 init process, request type: {request_type:?}, destination address: {dest_address:?}"
         );
 
+        match request_type {
+            message::Socks5InitCommandType::Connect => {
+                Self::handle_connect_command(
+                    client_socket_address.into(),
+                    dest_address.into(),
+                    proxy_connection_pool,
+                    init_framed,
+                    client_socket_address,
+                    configuration,
+                )
+                .await?;
+            },
+            message::Socks5InitCommandType::Bind => todo!(),
+            message::Socks5InitCommandType::UdpAssociate => todo!(),
+        }
+
+        Ok(())
+    }
+
+    async fn handle_connect_command(
+        src_address: PpaassNetAddress, dest_address: PpaassNetAddress, proxy_connection_pool: Arc<ProxyConnectionPool>,
+        mut init_framed: Framed<T, Socks5InitCommandContentCodec>, client_socket_address: SocketAddr, configuration: Arc<AgentServerConfig>,
+    ) -> Result<(), anyhow::Error> {
         let user_token = configuration
             .get_user_token()
             .as_ref()
@@ -99,104 +122,83 @@ where
                 "Client tcp connection [{client_socket_address}] can not get user token form configuration file"
             ))?
             .clone();
-        let src_address: PpaassNetAddress = self.client_socket_address.into();
-        let dest_address: PpaassNetAddress = dest_address.into();
+
         let payload_encryption = AgentServerPayloadEncryptionTypeSelector::select(&user_token, Some(generate_uuid().into_bytes()));
-        match request_type {
-            message::Socks5InitCommandType::Connect => {
-                let tcp_loop_init_request =
-                    PpaassMessageGenerator::generate_tcp_loop_init_request(&user_token, src_address.clone(), dest_address.clone(), payload_encryption.clone())?;
-                let proxy_connection = proxy_connection_pool.take_connection().await.context(format!(
-                    "Client tcp connection [{client_socket_address}] fail to take proxy connection from connection poool because of error"
-                ))?;
-
-                let proxy_connection_id = proxy_connection.id.clone();
-                let (mut proxy_connection_read, mut proxy_connection_write) = proxy_connection.split()?;
-
-                debug!("Client tcp connection [{client_socket_address}] take proxy connectopn [{proxy_connection_id}] to do proxy");
-
-                if let Err(e) = proxy_connection_write.send(tcp_loop_init_request).await {
-                    error!("Client tcp connection [{client_socket_address}] fail to send tcp loop init to proxy because of error: {e:?}");
-                    return Err(anyhow::anyhow!(format!(
-                        "Client tcp connection [{client_socket_address}] fail to send tcp loop init request to proxy because of error: {e:?}"
-                    )));
-                };
-
-                let proxy_message = proxy_connection_read
-                    .next()
-                    .await
-                    .context(format!(
-                        "Client tcp connection [{client_socket_address}] nothing to read from proxy for init tcp loop response"
-                    ))?
-                    .context(format!(
-                        "Client tcp connection [{client_socket_address}] error happen when read proxy message for init tcp loop response"
-                    ))?;
-
-                let PpaassMessageParts {
-                    payload_bytes: proxy_message_payload_bytes,
-                    user_token,
-                    ..
-                } = proxy_message.split();
-                let PpaassMessageProxyPayloadParts { payload_type, data } =
-                    TryInto::<PpaassMessageProxyPayload>::try_into(proxy_message_payload_bytes)?.split();
-                let tcp_loop_init_response = match payload_type {
-                    PpaassMessageProxyPayloadType::TcpLoopInit => TryInto::<TcpLoopInitResponsePayload>::try_into(data)?,
-                    _ => {
-                        error!("Client tcp connection [{client_socket_address}] receive invalid message from proxy, payload type: {payload_type:?}");
-                        return Err(anyhow::anyhow!(format!(
-                            "Client tcp connection [{client_socket_address}] receive invalid message from proxy, payload type: {payload_type:?}"
-                        )));
-                    },
-                };
-
-                let TcpLoopInitResponsePayload {
-                    loop_key: tcp_loop_key,
-                    dest_address,
-                    response_type,
-                    ..
-                } = tcp_loop_init_response;
-
-                match response_type {
-                    TcpLoopInitResponseType::Success => {
-                        debug!("Client tcp connection [{client_socket_address}] receive init tcp loop init response: {tcp_loop_key}");
-                    },
-                    TcpLoopInitResponseType::Fail => {
-                        error!("Client tcp connection [{client_socket_address}] fail to do tcp loop init, tcp loop key: [{tcp_loop_key}]");
-                        return Err(anyhow::anyhow!(format!(
-                            "Client tcp connection [{client_socket_address}] fail to do tcp loop init, tcp loop key: [{tcp_loop_key}]"
-                        )));
-                    },
-                }
-
-                let socks5_init_success_result =
-                    Socks5InitCommandResultContent::new(Socks5InitCommandResultStatus::Succeeded, Some(dest_address.clone().into()));
-
-                if let Err(e) = init_framed.send(socks5_init_success_result).await {
-                    error!("Client tcp connection [{client_socket_address}] fail reply init success in socks5 flow, tcp loop key: [{tcp_loop_key}].");
-                    return Err(e);
-                };
-
-                let FramedParts { io: client_io, .. } = init_framed.into_parts();
-                debug!("Client tcp connection [{client_socket_address}] success to do sock5 handshake begin to relay, tcp loop key: [{tcp_loop_key}].");
-
-                ClientFlow::relay(ClientDataRelayInfo {
-                    client_io,
-                    client_socket_address,
-                    tcp_loop_key: tcp_loop_key.clone(),
-                    user_token,
-                    payload_encryption,
-                    proxy_connection_read,
-                    proxy_connection_write,
-                    configuration,
-                    init_data: None,
-                })
-                .await?;
-                debug!("Client tcp connection [{client_socket_address}] complete sock5 relay, tcp loop key: [{tcp_loop_key}].");
+        let tcp_loop_init_request =
+            PpaassMessageGenerator::generate_tcp_loop_init_request(&user_token, src_address.clone(), dest_address.clone(), payload_encryption.clone())?;
+        let proxy_connection = proxy_connection_pool.take_connection().await.context(format!(
+            "Client tcp connection [{client_socket_address}] fail to take proxy connection from connection poool because of error"
+        ))?;
+        let proxy_connection_id = proxy_connection.id.clone();
+        let (mut proxy_connection_read, mut proxy_connection_write) = proxy_connection.split()?;
+        debug!("Client tcp connection [{client_socket_address}] take proxy connectopn [{proxy_connection_id}] to do proxy");
+        if let Err(e) = proxy_connection_write.send(tcp_loop_init_request).await {
+            error!("Client tcp connection [{client_socket_address}] fail to send tcp loop init to proxy because of error: {e:?}");
+            return Err(anyhow::anyhow!(format!(
+                "Client tcp connection [{client_socket_address}] fail to send tcp loop init request to proxy because of error: {e:?}"
+            )));
+        };
+        let proxy_message = proxy_connection_read
+            .next()
+            .await
+            .context(format!(
+                "Client tcp connection [{client_socket_address}] nothing to read from proxy for init tcp loop response"
+            ))?
+            .context(format!(
+                "Client tcp connection [{client_socket_address}] error happen when read proxy message for init tcp loop response"
+            ))?;
+        let PpaassMessageParts {
+            payload_bytes: proxy_message_payload_bytes,
+            user_token,
+            ..
+        } = proxy_message.split();
+        let PpaassMessageProxyPayloadParts { payload_type, data } = TryInto::<PpaassMessageProxyPayload>::try_into(proxy_message_payload_bytes)?.split();
+        let tcp_loop_init_response = match payload_type {
+            PpaassMessageProxyPayloadType::TcpLoopInit => TryInto::<TcpLoopInitResponsePayload>::try_into(data)?,
+            _ => {
+                error!("Client tcp connection [{client_socket_address}] receive invalid message from proxy, payload type: {payload_type:?}");
+                return Err(anyhow::anyhow!(format!(
+                    "Client tcp connection [{client_socket_address}] receive invalid message from proxy, payload type: {payload_type:?}"
+                )));
             },
-            message::Socks5InitCommandType::Bind => todo!(),
-            message::Socks5InitCommandType::UdpAssociate => todo!(),
+        };
+        let TcpLoopInitResponsePayload {
+            loop_key: tcp_loop_key,
+            dest_address,
+            response_type,
+            ..
+        } = tcp_loop_init_response;
+        match response_type {
+            TcpLoopInitResponseType::Success => {
+                debug!("Client tcp connection [{client_socket_address}] receive init tcp loop init response: {tcp_loop_key}");
+            },
+            TcpLoopInitResponseType::Fail => {
+                error!("Client tcp connection [{client_socket_address}] fail to do tcp loop init, tcp loop key: [{tcp_loop_key}]");
+                return Err(anyhow::anyhow!(format!(
+                    "Client tcp connection [{client_socket_address}] fail to do tcp loop init, tcp loop key: [{tcp_loop_key}]"
+                )));
+            },
         }
-
+        let socks5_init_success_result = Socks5InitCommandResultContent::new(Socks5InitCommandResultStatus::Succeeded, Some(dest_address.clone().into()));
+        if let Err(e) = init_framed.send(socks5_init_success_result).await {
+            error!("Client tcp connection [{client_socket_address}] fail reply init success in socks5 flow, tcp loop key: [{tcp_loop_key}].");
+            return Err(e);
+        };
+        let FramedParts { io: client_io, .. } = init_framed.into_parts();
+        debug!("Client tcp connection [{client_socket_address}] success to do sock5 handshake begin to relay, tcp loop key: [{tcp_loop_key}].");
+        ClientFlow::relay(ClientDataRelayInfo {
+            client_io,
+            client_socket_address,
+            tcp_loop_key: tcp_loop_key.clone(),
+            user_token,
+            payload_encryption,
+            proxy_connection_read,
+            proxy_connection_write,
+            configuration,
+            init_data: None,
+        })
+        .await?;
+        debug!("Client tcp connection [{client_socket_address}] complete sock5 relay, tcp loop key: [{tcp_loop_key}].");
         Ok(())
     }
 }
