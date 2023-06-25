be24a19c4fdcfbd46fb54ef23927ec9f6a52b36a
quhxuxm <quhxuxm.quh@gmail.com>
2023-06-26 07:24:40 +0800

    refact

--- proxy/src/processor/tcp/destination.rs
+++ proxy/src/processor/tcp/destination.rs
@@ -2,11 +2,8 @@ use std::pin::Pin;
 use std::task::{Context, Poll};
 
 use bytes::BytesMut;
-use futures::{
-    stream::{SplitSink, SplitStream},
-    Sink, StreamExt,
-};
-use futures_util::Stream;
+
+use futures::{Sink, Stream};
 use pin_project::pin_project;
 
 use tokio::io::{AsyncRead, AsyncWrite};
@@ -14,25 +11,14 @@ use tokio_util::codec::{BytesCodec, Framed};
 
 use crate::error::NetworkError;
 
-type DstBytesFramedRead<T> = SplitStream<Framed<T, BytesCodec>>;
-type DstBytesFramedWrite<T> = SplitSink<Framed<T, BytesCodec>, BytesMut>;
-
-/// The parts of the destination connection
-pub(crate) struct DstConnectionParts<T>
-where
-    T: AsyncRead + AsyncWrite + Unpin + Send + Sync + 'static,
-{
-    pub read: DstConnectionRead<T>,
-    pub write: DstConnectionWrite<T>,
-}
-
 /// The destination connection framed with BytesCodec
+#[pin_project]
 pub(crate) struct DstConnection<T>
 where
     T: AsyncRead + AsyncWrite + Unpin + Send + Sync + 'static,
 {
-    framed_read: DstBytesFramedRead<T>,
-    framed_write: DstBytesFramedWrite<T>,
+    #[pin]
+    inner: Framed<T, BytesCodec>,
 }
 
 impl<T> DstConnection<T>
@@ -40,37 +26,12 @@ where
     T: AsyncRead + AsyncWrite + Unpin + Send + Sync + 'static,
 {
     pub fn new(stream: T, buffer_size: usize) -> Self {
-        let dst_bytes_framed = Framed::with_capacity(stream, BytesCodec::new(), buffer_size);
-        let (framed_write, framed_read) = dst_bytes_framed.split::<BytesMut>();
-        Self { framed_write, framed_read }
-    }
-
-    pub fn split(self) -> DstConnectionParts<T> {
-        let read = DstConnectionRead::new(self.framed_read);
-        let write = DstConnectionWrite::new(self.framed_write);
-        DstConnectionParts { read, write }
+        let inner = Framed::with_capacity(stream, BytesCodec::new(), buffer_size);
+        Self { inner }
     }
 }
 
-#[pin_project]
-pub(crate) struct DstConnectionWrite<T>
-where
-    T: AsyncRead + AsyncWrite + Unpin + Send + Sync + 'static,
-{
-    #[pin]
-    framed_write: DstBytesFramedWrite<T>,
-}
-
-impl<T> DstConnectionWrite<T>
-where
-    T: AsyncRead + AsyncWrite + Unpin + Send + Sync + 'static,
-{
-    fn new(framed_write: DstBytesFramedWrite<T>) -> Self {
-        Self { framed_write }
-    }
-}
-
-impl<T> Sink<BytesMut> for DstConnectionWrite<T>
+impl<T> Sink<BytesMut> for DstConnection<T>
 where
     T: AsyncRead + AsyncWrite + Unpin + Send + Sync + 'static,
 {
@@ -78,44 +39,26 @@ where
 
     fn poll_ready(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
         let this = self.project();
-        this.framed_write.poll_ready(cx).map_err(NetworkError::DestinationWrite)
+        Sink::<BytesMut>::poll_ready(this.inner, cx).map_err(NetworkError::DestinationWrite)
     }
 
     fn start_send(self: Pin<&mut Self>, item: BytesMut) -> Result<(), Self::Error> {
         let this = self.project();
-        this.framed_write.start_send(item).map_err(NetworkError::DestinationWrite)
+        Sink::<BytesMut>::start_send(this.inner, item).map_err(NetworkError::DestinationWrite)
     }
 
     fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
         let this = self.project();
-        this.framed_write.poll_flush(cx).map_err(NetworkError::DestinationWrite)
+        Sink::<BytesMut>::poll_flush(this.inner, cx).map_err(NetworkError::DestinationWrite)
     }
 
     fn poll_close(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
         let this = self.project();
-        this.framed_write.poll_close(cx).map_err(NetworkError::DestinationWrite)
+        Sink::<BytesMut>::poll_close(this.inner, cx).map_err(NetworkError::DestinationWrite)
     }
 }
 
-#[pin_project]
-pub(crate) struct DstConnectionRead<T>
-where
-    T: AsyncRead + AsyncWrite + Unpin + Send + Sync + 'static,
-{
-    #[pin]
-    framed_read: DstBytesFramedRead<T>,
-}
-
-impl<T> DstConnectionRead<T>
-where
-    T: AsyncRead + AsyncWrite + Unpin + Send + Sync + 'static,
-{
-    fn new(framed_read: DstBytesFramedRead<T>) -> Self {
-        Self { framed_read }
-    }
-}
-
-impl<T> Stream for DstConnectionRead<T>
+impl<T> Stream for DstConnection<T>
 where
     T: AsyncRead + AsyncWrite + Unpin + Send + Sync + 'static,
 {
@@ -123,6 +66,6 @@ where
 
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
         let this = self.project();
-        this.framed_read.poll_next(cx).map_err(NetworkError::DestinationRead)
+        this.inner.poll_next(cx).map_err(NetworkError::DestinationRead)
     }
 }
--- proxy/src/processor/tcp/handler.rs
+++ proxy/src/processor/tcp/handler.rs
@@ -33,7 +33,7 @@ use crate::{
     error::{NetworkError, ProxyError},
 };
 
-use super::destination::{DstConnection, DstConnectionParts, DstConnectionRead, DstConnectionWrite};
+use super::destination::DstConnection;
 
 #[derive(Debug, Clone, Constructor, Display)]
 #[display(fmt = "[{}]#[{}]@TCP::[{}]::[{}=>{}]", connection_id, user_token, agent_address, src_address, dst_address)]
@@ -64,9 +64,7 @@ where
     R: RsaCryptoFetcher + Send + Sync + 'static,
     I: AsRef<str> + Send + Sync + Clone + Display + Debug + 'static,
 {
-    async fn init_dst_connection(
-        handler_key: &TcpHandlerKey, configuration: Arc<ProxyServerConfig>,
-    ) -> Result<(DstConnectionRead<TcpStream>, DstConnectionWrite<TcpStream>), ProxyError> {
+    async fn init_dst_connection(handler_key: &TcpHandlerKey, configuration: Arc<ProxyServerConfig>) -> Result<DstConnection<TcpStream>, ProxyError> {
         let dst_socket_address = handler_key.dst_address.to_socket_addrs()?.collect::<Vec<SocketAddr>>();
 
         let dst_tcp_stream = match timeout(
@@ -94,12 +92,7 @@ where
         dst_tcp_stream.set_nodelay(true)?;
         dst_tcp_stream.set_linger(None)?;
         let dst_connection = DstConnection::new(dst_tcp_stream, configuration.get_dst_tcp_buffer_size());
-        let DstConnectionParts {
-            read: dst_tcp_read,
-            write: dst_tcp_write,
-            ..
-        } = dst_connection.split();
-        Ok((dst_tcp_read, dst_tcp_write))
+        Ok(dst_connection)
     }
 
     fn unwrap_to_raw_tcp_data(message: PpaassMessage) -> Result<Vec<u8>, CommonError> {
@@ -114,7 +107,7 @@ where
         let dst_relay_timeout = self.configuration.get_dst_relay_timeout();
         let agent_relay_timeout = self.configuration.get_agent_relay_timeout();
 
-        let (dst_connection_read, mut dst_connection_write) = match Self::init_dst_connection(&handler_key, self.configuration).await {
+        let dst_connection = match Self::init_dst_connection(&handler_key, self.configuration).await {
             Ok(dst_read_and_write) => dst_read_and_write,
             Err(e) => {
                 let payload_encryption = ProxyServerPayloadEncryptionSelector::select(&handler_key.user_token, Some(generate_uuid().into_bytes()));
@@ -142,6 +135,7 @@ where
         agent_connection.send(tcp_init_success_message).await?;
         debug!("Tcp handler {handler_key} create destination connection success.");
         let (mut agent_connection_write, agent_connection_read) = agent_connection.split();
+        let (mut dst_connection_write, dst_connection_read) = dst_connection.split();
         {
             let handler_key = handler_key.clone();
             tokio::spawn(async move {
