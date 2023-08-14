0fbc3d61e14e46c5b9ba423f16260f4b53a7f6be
quhxuxm <quhxuxm.quh@gmail.com>
2023-08-14 18:34:00 +0800

    refact

--- agent/src/main.rs
+++ agent/src/main.rs
@@ -6,13 +6,21 @@ pub(crate) mod error;
 pub(crate) mod pool;
 pub(crate) mod processor;
 
+use chrono::Local;
 use config::AGENT_CONFIG;
 use ppaass_common::PpaassMessagePayloadEncryptionSelector;
 
 use anyhow::Result;
-use log::error;
+use log::{error, LevelFilter as LogLevelFilter};
 use server::AgentServer;
 use tokio::runtime::Builder;
+use tracing::metadata::LevelFilter as TracingLevelFilter;
+use tracing_log::LogTracer;
+use tracing_subscriber::{
+    fmt::{format::Writer, time::FormatTime, Layer},
+    prelude::__tracing_subscriber_SubscriberExt,
+    Registry,
+};
 
 pub const SOCKS_V5: u8 = 5;
 pub const SOCKS_V4: u8 = 4;
@@ -21,8 +29,36 @@ pub struct AgentServerPayloadEncryptionTypeSelector;
 
 impl PpaassMessagePayloadEncryptionSelector for AgentServerPayloadEncryptionTypeSelector {}
 
+pub struct LogTimer;
+
+impl FormatTime for LogTimer {
+    fn format_time(&self, w: &mut Writer<'_>) -> std::fmt::Result {
+        write!(w, "{}", Local::now().format("%FT%T%.3f"))
+    }
+}
+
 fn main() -> Result<()> {
-    log4rs::init_file("resources/config/ppaass-agent-log.yaml", Default::default())?;
+    let log_dir_path = "log";
+    let log_file_path = "ppaass-agent.log";
+    let file_appender = tracing_appender::rolling::daily(log_dir_path, log_file_path);
+    let (non_blocking, _appender_guard) = tracing_appender::non_blocking(file_appender);
+
+    let subscriber = Registry::default()
+        .with(
+            Layer::default()
+                .with_level(true)
+                .with_target(true)
+                .with_timer(LogTimer)
+                .with_thread_ids(true)
+                .with_file(true)
+                .with_ansi(false)
+                .with_line_number(true)
+                .with_writer(non_blocking),
+        )
+        .with(TracingLevelFilter::ERROR);
+    tracing::subscriber::set_global_default(subscriber).expect("Fail to initialize tracing subscriber");
+    LogTracer::builder().with_max_level(LogLevelFilter::Error).init()?;
+
     let agent_server_runtime = Builder::new_multi_thread()
         .enable_all()
         .worker_threads(AGENT_CONFIG.get_worker_thread_number())
--- proxy/src/main.rs
+++ proxy/src/main.rs
@@ -5,17 +5,54 @@ mod error;
 mod processor;
 mod server;
 
+use chrono::Local;
 use config::PROXY_CONFIG;
-use log::error;
 use log::info;
+use log::{error, LevelFilter as LogLevelFilter};
 
 use anyhow::Result;
 use tokio::runtime::Builder;
 
+use tracing_log::LogTracer;
+use tracing_subscriber::{
+    fmt::{format::Writer, time::FormatTime, Layer},
+    prelude::__tracing_subscriber_SubscriberExt,
+    Registry,
+};
+
+use tracing::metadata::LevelFilter as TracingLevelFilter;
+
 use crate::server::ProxyServer;
 
+pub struct LogTimer;
+
+impl FormatTime for LogTimer {
+    fn format_time(&self, w: &mut Writer<'_>) -> std::fmt::Result {
+        write!(w, "{}", Local::now().format("%FT%T%.3f"))
+    }
+}
+
 fn main() -> Result<()> {
-    log4rs::init_file("resources/config/ppaass-proxy-log.yaml", Default::default())?;
+    let log_dir_path = "log";
+    let log_file_path = "ppaass-proxy.log";
+    let file_appender = tracing_appender::rolling::daily(log_dir_path, log_file_path);
+    let (non_blocking, _appender_guard) = tracing_appender::non_blocking(file_appender);
+
+    let subscriber = Registry::default()
+        .with(
+            Layer::default()
+                .with_level(true)
+                .with_target(true)
+                .with_timer(LogTimer)
+                .with_thread_ids(true)
+                .with_file(true)
+                .with_ansi(false)
+                .with_line_number(true)
+                .with_writer(non_blocking),
+        )
+        .with(TracingLevelFilter::ERROR);
+    tracing::subscriber::set_global_default(subscriber).expect("Fail to initialize tracing subscriber");
+    LogTracer::builder().with_max_level(LogLevelFilter::Error).init()?;
 
     let proxy_server_runtime = Builder::new_multi_thread()
         .enable_all()
